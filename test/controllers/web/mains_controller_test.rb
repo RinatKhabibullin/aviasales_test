22b1a073c18028b3320d077057fa278e2fd78a51
RinatKhabibullin <lmiksl@gmail.com>
2021-08-09 18:10:06 +0000

    fix solargraph

--- test/controllers/web/mains_controller_test.rb
+++ test/controllers/web/mains_controller_test.rb
@@ -1,7 +1,7 @@
-require "test_helper"
+require 'test_helper'
 
 class Web::MainsControllerTest < ActionController::TestCase
-  test "should get new" do
+  test 'should get new' do
     get :show
     assert_response :success
   end
